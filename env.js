1475b442cdf68e47733b025fec91f045f691accd
Minhaj Shah <147550750+minhajshah23@users.noreply.github.com>
2026-04-26 15:55:57 +0500

    deleted useless files

--- env.js
+++
@@ -1,5 +0,0 @@
-// ✅ ATTACH TO WINDOW so script.js can see it
-window.ENV = {
-    GROQ_SECRET: "d3NrdVNKb1cxRU4wNmNKaGlyb1hPRjk0WUYzYnlkR1dBcFp3VWxGRkpMZlRVeGttdzJGUF9rc2c", 
-    TAVILY_SECRET: "NVFXQnJTS3JCUWZJRURSM3ltdkhVd3NYeDhLVVRyWmstdmVkLXlsdnQ"
-};
